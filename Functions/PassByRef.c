7f0decd9f5580b0939ae338356b6e46ce43bb786
OtavioRMC <otavioribeiromoreiradacosta@gmail.com>
2023-02-23 17:06:12 -0300

    .

--- Functions/PassByRef.c
+++ Functions/PassByRef.c
@@ -9,11 +9,14 @@ int main(){
   b = 5;
   printf("&b : %p",&b);
   add_one(&b);
+  printf("\n");
   printf("b: %i\n",b);
+  
 
   return 0;
 }
 void add_one(int *a){
-  printf("a : %p", a);
+  printf("\n");
+  printf("a : %p ", a);
   *a = *a + 1;
 }
