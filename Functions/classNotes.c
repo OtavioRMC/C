98498564967bb459a3de9b599a0f6f6f852f0774
OtávioRMC <otavioribeiromoreiradacosta@gmail.com>
2023-03-10 08:01:38 -0300

    Class Notes

---
+++ Functions/classNotes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*  Funções
+        *Principio da responsabilidade única.
+        *Evitar 'fat' procedures
+        ou seja funções que tem muitas responsabilidades.
+        alta coesão e baixo acoplamento.
+        
+    */
+
+
+
+    return 0;
+}
