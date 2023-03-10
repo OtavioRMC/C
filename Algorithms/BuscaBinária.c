911102a668d8632ef91cfd1aca8a6e3d80760060
OtávioRMC <otavioribeiromoreiradacosta@gmail.com>
2023-03-10 07:46:48 -0300

    Binary Search Algorithm

--- Algorithms/BuscaBinária.c
+++ Algorithms/BuscaBinária.c
@@ -10,13 +10,13 @@ int main(){
     int vet[] = {1,5,15,20,24,25,45,67,76}; //x=15.
     int resultado;
 
-    resultado = busca_binaria(vet,0,8,22);
+    resultado = binarySearch(vet,0,8,22);
     printf("%d",resultado);
     
     return 0;
 
 }
-int busca_binaria(int *dados, int l, int r, int x){
+int binarySearch(int *dados, int l, int r, int x){
 
 int m = (l + r) / 2; //l é a posição 0
 
@@ -25,8 +25,8 @@ if(l>r) return -1; /*caso base*/
 if(dados[m]==x) return m; /*caso base*/
 
 else if(x > dados[m])
-    return busca_binaria(dados,m+1,r,x);
+    return binarySearch(dados,m+1,r,x); //Caso Recursivo
 else 
-    return busca_binaria(dados,l,m-1,x);    
+    return binarySearch(dados,l,m-1,x); // Caso Recursivo   
 }
 
