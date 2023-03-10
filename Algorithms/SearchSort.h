4eb2e464a21c59dcc39d47cc066ef4f483453678
OtávioRMC <otavioribeiromoreiradacosta@gmail.com>
2023-03-10 07:55:39 -0300

    Algorithms Lib for data structures

--- Algorithms/SearchSort.h
+++ Algorithms/SearchSort.h
@@ -1,15 +1,14 @@
-//Biblioteca de algoritmos de ordenação e busca sequencial e binária.
 
-void troca(int *a,int *b){
+void changeVarValue(int *varOne,int *varTwo){
 
-    int aux;
-    aux = *a;
-    *a = *b;
-    *b = aux;
+    int auxiliary;
+    auxiliary = *varOne;
+    *varOne = *varTwo;
+    *varTwo = auxiliary;
     
 }
 
-void bubble_sort(int *v,int n){
+void bubbleSort(int *v,int n){
     int i,j;
     int aux;
     for(i = 0; i < n - 1; ++i)
@@ -27,7 +26,7 @@ void bubble_sort(int *v,int n){
     }
 }    
 
-void insertion_sort(int *v, int n){
+void insertionSort(int *v, int n){
 
     int i,j;
     int aux;
@@ -36,7 +35,7 @@ void insertion_sort(int *v, int n){
         for(j = i; j > 0; --j){
             if(v[j-1]>v[j]){
 
-                 troca(&v[j],&v[j-1]);
+                 changeVarValue(&v[j],&v[j-1]);
             }
         }
     }
@@ -44,16 +43,16 @@ void insertion_sort(int *v, int n){
 
 }
 
-int busca_sequencial(int *dados,int n, int x){
+int sequencialSearch(int *data,int n, int x){
     int i;
     for(i = 0; i < n; i++)
-        if(dados[i]==x)
+        if(data[i]==x)
             return i;
             
     return -1;
 }
 
-int busca_binaria(int *dados, int l, int r, int x){
+int binarySearch(int *dados, int l, int r, int x){
 
 int m = (l + r) / 2; //l é a posição 0
 
@@ -62,7 +61,7 @@ if(l>r) return -1; /*caso base*/
 if(dados[m]==x) return m; /*caso base*/
 
 else if(x > dados[m])
-    return busca_binaria(dados,m+1,r,x);
+    return binarySearch(dados,m+1,r,x);
 else 
-    return busca_binaria(dados,l,m-1,x);    
+    return binarySearch(dados,l,m-1,x);    
 }
